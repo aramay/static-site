32b19f7bcd6fbb4a75413c4c9ee1f4e788ce1109
“kasper341” <kasper_341@hotmail.com>
2016-08-30 16:20:03 -0700

    Add js to update main-content dynamically

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ $(function (){
 
     });
 });
+
+(function(global){
+    var dc = {};
+
+    var homeHtml = 'js/snippets/home-snippet.html';
+
+    // convinence function
+    var insertHtml = function(html, selector){
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHtml = html;
+    };
+
+    // On page load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function (event) {
+
+    // On first load, show home view
+    // showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+      homeHtml,
+      function (responseText) {
+        document.querySelector("#main-content")
+          .innerHTML = responseText.response;
+      },
+      false);
+    });
+
+
+    global.$dc = dc;
+
+})(window);
