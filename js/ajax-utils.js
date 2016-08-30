f0596a12d3e24d36493e0c48caea36fd17a9dd70
“kasper341” <kasper_341@hotmail.com>
2016-08-30 16:20:42 -0700

    Add ajax-utils method

---
+++ js/ajax-utils.js
@@ -0,0 +1,42 @@
+(function(global){
+    var ajaxUtils = {};
+
+    // Returns an HTTP request object
+    function getRequestObject(){
+        if (window.XMLHttpRequest) {
+            return (new XMLHttpRequest());
+        }
+        else if(window.ActiveObject){
+            return (new ActiveObject("Microsoft.XMLHTTP"));
+        }
+        else{
+            global.alert("Ajax is not supported!");
+            return(null);
+        }
+    }
+
+    // Make an ajax request
+    ajaxUtils.sendGetRequest = function(requestUrl, responseHandler){
+
+        var request = getRequestObject();
+
+        request.onreadystatechange = function(){
+            handleResponse(request, responseHandler);
+        };
+
+        request.open("GET", requestUrl, true);
+        request.send(null);
+
+    };
+
+    function handleResponse(request, responseHandler) {
+        if (request.readyState == 4 && request.status == 200) {
+            responseHandler(request);
+        }
+    }
+
+    // Expose utility to the global object
+    global.$ajaxUtils = ajaxUtils;
+    // console.log(ajaxUtils);
+
+}(window));
