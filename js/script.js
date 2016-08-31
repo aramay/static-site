db57783156fd45cb9b0542dd0ad0e941797e9ddd
“kasper341” <kasper_341@hotmail.com>
2016-08-30 18:56:32 -0700

    add methods to load categories template dynamically

--- js/script.js
+++ js/script.js
@@ -13,11 +13,23 @@ $(function (){
 
     var homeHtml = 'js/snippets/home-snippet.html';
 
+    var allCategoryUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+    var categoriesTitleHtml = "js/snippets/categories-title-snippet.html";
+    var categoryHtml = "js/snippets/categories-snippet.html";
+
     // convinence function
     var insertHtml = function(html, selector){
         var targetElem = document.querySelector(selector);
         targetElem.innerHtml = html;
     };
+
+    // Return substitute of '{{propName}}'
+    // with propValue in given 'string'
+    var insertProperty = function (string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    string = string.replace(new RegExp(propToReplace, "g"), propValue);
+      return string;
+  }
 
     // On page load (before images or CSS)
     document.addEventListener("DOMContentLoaded", function (event) {
@@ -32,6 +44,13 @@ $(function (){
       },
       false);
     });
+
+    // Load Menu Categories
+    dc.loadMenuCategories = function(){
+        $ajaxUtils.sendGetRequest(
+            allCategoryUrl,
+            buildAndShowCategoriesHTML);
+    };
 
 
     global.$dc = dc;
