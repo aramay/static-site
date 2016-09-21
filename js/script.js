62c8837ca8783b56b1babefafffe336a92938461
“kasper341” <kasper_341@hotmail.com>
2016-09-20 22:07:52 -0700

    Add code to display categories page

--- js/script.js
+++ js/script.js
@@ -9,13 +9,16 @@ $(function (){
 });
 
 (function(global){
+
+
+
     var dc = {};
 
-    var homeHtml = 'js/snippets/home-snippet.html';
+    var homeHtml = '../snippets/home-snippet.html';
 
     var allCategoryUrl = "https://davids-restaurant.herokuapp.com/categories.json";
-    var categoriesTitleHtml = "js/snippets/categories-title-snippet.html";
-    var categoryHtml = "js/snippets/categories-snippet.html";
+    var categoriesTitleHtml = "../snippets/categories-title-snippet.html";
+    var categoryHtml = "../snippets/categories-snippet.html";
 
     // convinence function
     var insertHtml = function(html, selector){
@@ -29,13 +32,15 @@ $(function (){
     var propToReplace = "{{" + propName + "}}";
     string = string.replace(new RegExp(propToReplace, "g"), propValue);
       return string;
-  }
+  };
 
     // On page load (before images or CSS)
     document.addEventListener("DOMContentLoaded", function (event) {
 
     // On first load, show home view
     // showLoading("#main-content");
+    // debugger;
+
     $ajaxUtils.sendGetRequest(
       homeHtml,
       function (responseText) {
@@ -52,7 +57,56 @@ $(function (){
             buildAndShowCategoriesHTML);
     };
 
+    // Builds HTML for the categories page based on the data
+    // from the server
+        function buildAndShowCategoriesHTML (categories) {
+            // Load title snippet of categories page
+            $ajaxUtils.sendGetRequest(
+                categoriesTitleHtml,
+                function (categoriesTitleHtml) {
+                    // Retrieve single category snippet
+                    $ajaxUtils.sendGetRequest(
+                        categoryHtml,
+                        function (categoryHtml) {
+                            var categoriesViewHtml =
+                            buildCategoriesViewHtml(categories,
+                                        categoriesTitleHtml,
+                                        categoryHtml);
+                                        insertHtml("#main-content", categoriesViewHtml);
+                                    },
+                                    false);
+                                },
+                                false);
+                            }
+
+                            // Using categories data and snippets html
+                        // build categories view HTML to be inserted into page
+                        function buildCategoriesViewHtml(categories,
+                                                         categoriesTitleHtml,
+                                                         categoryHtml) {
+
+                          var finalHtml = categoriesTitleHtml;
+                          finalHtml += "<section class='row'>";
+
+                          // Loop over categories
+                          for (var i = 0; i < categories.length; i++) {
+                            // Insert category values
+                            var html = categoryHtml;
+                            var name = "" + categories[i].name;
+                            var short_name = categories[i].short_name;
+                            html =
+                              insertProperty(html, "name", name);
+                            html =
+                              insertProperty(html,
+                                             "short_name",
+                                             short_name);
+                            finalHtml += html;
+                          }
+
+                          finalHtml += "</section>";
+                          return finalHtml;
+                        }
 
     global.$dc = dc;
 
-})(window);
+}) (window);
