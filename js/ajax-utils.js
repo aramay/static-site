e670d5c07471df43aa6f51b35f188ef71e3300ef
“kasper341” <kasper_341@hotmail.com>
2016-09-21 09:01:50 -0700

    fix  reference error

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -1,44 +1,51 @@
-(function (global){
+(function (global) {
 
-    var ajaxUtils = {};
-// debugger;
-    // Returns an HTTP request object
-    function getRequestObject(){
-        if (window.XMLHttpRequest) {
-            return (new XMLHttpRequest());
-        }
-        else if(window.ActiveObject){
-            return (new ActiveObject("Microsoft.XMLHTTP"));
-        }
-        else{
-            global.alert("Ajax is not supported!");
-            return(null);
-        }
-    }
+// Set up a namespace for our utility
+var ajaxUtils = {};
 
-    // Make an ajax request
-    ajaxUtils.sendGetRequest = function(requestUrl, responseHandler){
 
-        var request = getRequestObject();
+// Returns an HTTP request object
+function getRequestObject() {
+  if (window.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else if (window.ActiveXObject) {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
+  }
+}
 
-        request.onreadystatechange = function(){
-            handleResponse(request, responseHandler);
-        };
 
-        request.open("GET", requestUrl, true);
-        request.send(null);
+// Makes an Ajax GET request to 'requestUrl'
+ajaxUtils.sendGetRequest = 
+  function(requestUrl, responseHandler, isJsonResponse) {
+    var request = getRequestObject();
+    request.onreadystatechange = 
+      function() { 
+        handleResponse(request, 
+                       responseHandler,
+                       isJsonResponse); 
+      };
+    request.open("GET", requestUrl, true);
+    request.send(null); // for POST only
+  };
 
-    };
 
-    function handleResponse(request,
+// Only calls user provided 'responseHandler'
+// function if response is ready
+// and not an error
+function handleResponse(request,
                         responseHandler,
                         isJsonResponse) {
-
-        if ((request.readyState == 4) &&
-        (request.status == 200)) {
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
 
     // Default to isJsonResponse = true
-    if (isJsonResponse === undefined) {
+    if (isJsonResponse == undefined) {
       isJsonResponse = true;
     }
 
@@ -46,14 +53,15 @@
       responseHandler(JSON.parse(request.responseText));
     }
     else {
-      responseHandler(request);
+      responseHandler(request.responseText);
     }
   }
 }
 
-    // Expose utility to the global object
-    global.$ajaxUtils = ajaxUtils;
-    // console.log(global.$ajaxUtils);
-    console.log(window.$ajaxUtils);
 
-}) (window);
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+
+})(window);
+
